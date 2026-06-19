fe57f60b1bce8d9f1da6690191e661bd77e3c3eb
mschaub62 <matthew.schaub@snhu.edu>
2026-06-19 13:05:08 -0500

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -52,7 +52,7 @@
     }, 2600);
   }
 
-  // --- active section highlighting  ---
+  // --- active section highlighting (in-page anchors on home) ---
   var sectionLinks = Array.prototype.slice.call(
     document.querySelectorAll('.nav a[href^="#"]')
   );
